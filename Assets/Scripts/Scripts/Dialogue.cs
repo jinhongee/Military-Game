00bebece4d46f37295982ba53e125dd5b195f172
Jinhong Min <pappies0827@gmail.com>
2021-10-30 02:26:44 +0900

    Update and rename Assets/Scripts/Dialogue.cs to Assets/Scripts/Scripts/Dialogue.cs

---
+++ Assets/Scripts/Scripts/Dialogue.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    public GameObject DialogueBox;
+    public GameObject selection;
+    public GameObject black;
+    //public FadeToBlack fadeToBlack;
+    //private int index;
+
+    public DialogueFund dialogueFund;
+    private string scene = "Training";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueFund.InitiateProcess(textComponent, lines, textSpeed, DialogueBox, selection, black, scene);
+    }
+}
